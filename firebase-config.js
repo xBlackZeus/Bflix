40b9a0f746b0573f6d489e774d94c6ed5833ed4c
bflix <bflix@local>
2026-09-11 15:54:26 +0300

    release

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,1 @@
-window.FB_CONFIG={apiKey:"PASTE_ME",authDomain:"PASTE_ME",projectId:"PASTE_ME",appId:"PASTE_ME"};
+window.FB_CONFIG={apiKey:"AIzaSyCRP9uS-LGGtQHR6IAdKXfOoUk_7xuq16A",authDomain:"bflix-d191e.firebaseapp.com",projectId:"bflix-d191e",appId:"1:565487649821:web:04ca058ffda5c5e022f2f4"};
